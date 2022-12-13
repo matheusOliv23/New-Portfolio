220d544074648c1a2496f8f109ed6d5af7848b4e
matheusOliv23 <matheushenri1996@gmail.com>
2022-12-13 20:18:31 -0300

    refactor: refactor Hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,6 @@
 import Headline from "../Headline"
 import SocialIcons from "../SocialIcons"
+import { HeroImgStyles, HeroStyles, ImgContainer } from "./styles"
 
 export default function Hero() {
   return (
@@ -8,13 +9,13 @@ export default function Hero() {
         backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.9)), url('/images/ales-nesetril-opac.png')`,
         backgroundBlendMode: "lighten"
       }}
-      className="h-[90vh] transition-all duration-300 lg:flex-row flex-col-reverse flex items-center justify-around px-7 py-3 bg-no-repeat bg-fixed bg-cover bg-center"
+      className={HeroStyles}
     >
       <Headline />
-      <div className="flex flex-col items-center gap-5">
+      <div className={ImgContainer}>
         <div
           style={{ backgroundImage: "url('/images/eu.webp')" }}
-          className="w-[15.5rem] md:w-[22rem] md:h-[22rem] flex items-center justify-center bg-cover bg-center pr-[0.5rem] bg-no-repeat ring-[5px] ring-inset ring-teal-500 rounded-[50%] h-[15.5rem]"
+          className={HeroImgStyles}
         />
 
         <SocialIcons />
