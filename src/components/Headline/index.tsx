4c1946e43da913f2bf71071191e1054534fbc815
matheusOliv23 <matheushenri1996@gmail.com>
2022-12-13 20:14:46 -0300

    feat: refactor Headline

--- src/components/Headline/index.tsx
+++ src/components/Headline/index.tsx
@@ -1,18 +1,23 @@
 import Button from "../Button"
+import {
+  ButtonContainerStyls,
+  HeadlineContainer,
+  SubtitleStyles,
+  TextStyles,
+  TitleStyles
+} from "./styles"
 
 export default function Headline() {
   return (
-    <div className="flex flex-col lg:items-start z-50 gap-8 !opacity-100 items-center justify-center lg:text-start text-center">
-      <h1 className="font-bold md:text-5xl text-2xl tracking-widest ">
-        MATHEUS HENRIQUE OLIVEIRA
-      </h1>
-      <h2 className="lg:text-5xl text-4xl text-[#8a8f98] font-medium tracking-widest mt-2">
+    <div className={HeadlineContainer}>
+      <h1 className={TitleStyles}>MATHEUS HENRIQUE OLIVEIRA</h1>
+      <h2 className={SubtitleStyles}>
         Desenvolvedor <strong className="text-primary">Front-End</strong>
       </h2>
-      <h3 className="lg:text-3xl text-2xl max-w-[32rem]">
+      <h3 className={TextStyles}>
         Transformando sonhos em realidade por meio da tecnologia
       </h3>
-      <div className="flex gap-6 justify-center items-center">
+      <div className={ButtonContainerStyls}>
         <Button type="button" variant="PRIMARY">
           Quem sou eu
         </Button>
