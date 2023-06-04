baaf5e98075335dea3c74cdd2e013d2eab0e850d
matheusOliv23 <matheushenri1996@gmail.com>
2023-06-04 10:58:32 -0300

    feat: adjusting about section layout

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,60 +1,79 @@
 import React from "react"
 import { motion } from "framer-motion"
-
+import tech from "src/lotties/tech.json"
+import Lottie from "react-lottie"
 
 export default function About() {
+  const about = [
+    "Habilidades em HTML, CSS e JavaScript, com foco em criar designs responsivos e acessíveis.",
+    "Proficiente no uso de frameworks modernos de front-end, como o React, para construir interfaces de usuário dinâmicas e interativas.",
+    "Proativo e rápido aprendizado, com forte capacidade de adaptação.",
+    "Sempre em busca de novos desafios para expandir minhas habilidades e acompanhar o campo de desenvolvimento web, que está em constante evolução."
+  ]
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: tech,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  }
+
   return (
     <section
       id="about"
-      className="flex w-full lg:flex-row flex-col font-openSans gap-10 max-w-7xl px-7 py-6 md:py-20 lg:py-32 m-auto xl:justify-between lg:justify-center"
+      className="flex w-full flex-col bg-gray-light font-openSans gap-10 px-7 py-6 md:py-20 lg:py-32 m-auto"
     >
-      <div className="space-y-4">
-        <h2 className="text-2xl text-primary">Sobre mim</h2>
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          transition={{ duration: 0.3 }}
-          variants={{
-            visible: { opacity: 1, scale: 1 },
-            hidden: { opacity: 0, scale: 0 }
-          }}
-          className="lg:max-w-xl text-xl text-justify"
-        >
-          Tenho como objetivo principal me tornar destaque no desenvolvimento
-          web, principalmente na parte de Front-End. Sou uma pessoa muito
-          dedicada, educada, responsável e gosto muito de novos desafios.
-        </motion.div>
-      </div>
-
-      <div className="flex items-center justify-center flex-col w-full lg:max-w-sm space-y-4">
-        <div className="flex w-full justify-between md:gap-16 md:justify-start lg:justify-between items-center">
-          <div className="flex items-center gap-2">
-            <p className="text-6xl font-semibold text-primary">1.8</p>
-            <p className="text-sm">
-              anos <br /> de experiência
-            </p>
-          </div>
-          <div className="flex items-center gap-2">
-            <p className="text-6xl font-semibold text-primary">8</p>
-            <p className="text-sm">
-              projetos <br /> concluídos
-            </p>
-          </div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={{
+          visible: { opacity: 1, scale: 1 },
+          hidden: { opacity: 0, scale: 0 }
+        }}
+        transition={{
+          duration: 0.6,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01]
+        }}
+      >
+        <h2 className="text-[40px] text-center">Sobre Mim</h2>
+      </motion.div>
+      <div className="lg:flex-row lg:gap-6 items-center flex m-auto flex-col max-w-7xl xl:justify-between lg:justify-between">
+        <div className="space-y-4">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            transition={{ duration: 0.3 }}
+            variants={{
+              visible: { opacity: 1, scale: 1 },
+              hidden: { opacity: 0, scale: 0 }
+            }}
+            className="lg:max-w-xl text-xl text-justify"
+          ></motion.div>
+          <ul className="!list-disc space-y-4 md:pl-6">
+            {about.map((item, index) => (
+              <li key={index} className="list-disc">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  transition={{ duration: 0.8 }}
+                  variants={{
+                    visible: { opacity: 1, scale: 1 },
+                    hidden: { opacity: 0, scale: 0 }
+                  }}
+                  className="lg:max-w-xl text-justify"
+                >
+                  {item}
+                </motion.div>
+              </li>
+            ))}
+          </ul>
         </div>
-
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          transition={{ duration: 0.5 }}
-          variants={{
-            visible: { opacity: 1, scale: 1 },
-            hidden: { opacity: 0, scale: 0 }
-          }}
-          className="text-justify"
-        >
-          Experiência em desenvolvimento de aplicações web responsivas e
-          escaláveis utilizando Javascript, Typescript e Reactjs.
-        </motion.div>
+        <div>
+          <Lottie options={defaultOptions} height={500} width={500} />
+        </div>
       </div>
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx}",
-        "./src/components/**/*.{js,ts,jsx,tsx}",
-        // "./src/components/stories/**/*.{ts,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'primary': "#2068c7",
-                'secondary': "#7ac7e3",
-                'dark-200': "#09090b",
-                'dark-300': '#09090b'
-            },
-            fontFamily: {
-                dm: ['DM Sans', 'Roboto', 'sans-serif'],
-                quicksand: ['Quicksand', 'sans-serif'],
-                openSans: ['Open Sans', 'sans-serif']
-            },
-            boxShadow: {
-                '2xl': '0 0 35px #1e9bff'
-            }
-        },
-    },
-    plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+    // "./src/components/stories/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2068c7",
+        secondary: "#7ac7e3",
+        "dark-200": "#09090b",
+        "dark-300": "#09090b",
+        "gray-light": "#0d1117"
+      },
+      fontFamily: {
+        dm: ["DM Sans", "Roboto", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"]
+      },
+      boxShadow: {
+        "2xl": "0 0 35px #1e9bff"
+      }
+    }
+  },
+  plugins: []
 }
